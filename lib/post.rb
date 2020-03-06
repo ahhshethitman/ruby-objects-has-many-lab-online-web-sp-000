bdf8992f51d557931cae99ed4fb091c712258e90
Hunter Sides <hunter.s.1337@gmail.com>
2020-03-06 20:17:57 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -8,16 +8,20 @@ class Post
     @title = title
     @@all << self
   end
-   def author_name 
+  
+  def author_name 
      post_knows_author = self.author.name
      if post_knows_author == ""
-       return nil 
+       return nil
+     else 
+       post_knows_author
      end 
-     
-   end
+  end
+  
   def self.all 
     @@all
   end 
+
 end
 
 
